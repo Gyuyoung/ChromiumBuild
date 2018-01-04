4dc6326c3141f7d66ff873b8d80392004261246b
Gyuyoung Kim <gyuyoung.kim@lge.com>
2018-01-04 14:40:40 +0900

    Update buildChromium.sh

--- buildChromium.sh
+++ buildChromium.sh
@@ -11,15 +11,15 @@ export CCACHE_PREFIX=icecc
 export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
 export CCACHE_BASEDIR=$HOME/chromium
 export ICECC_CLANG_REMOTE_CPP=1
-export ICECC_VERSION=/home/gyuyoung/chromium/clang.tar.gz
-export PATH=/usr/lib/ccache:$PATH
-export PATH=/usr/lib/icecc/bin:$PATH
-export PATH=$HOME/chromium/chromium-trunk/src/third_party/llvm-build/Release+Asserts/bin:$PATH
+export ICECC_VERSION=$HOME/chromium/clang.tar.gz
+export CHROMIUM_SRC=$HOME/chromium/src # Please set the chromium src path in your local.
+export PATH=/usr/lib/ccache:/usr/lib/icecc/bin:$PATH
+export PATH=$CHROMIUM_SRC/third_party/llvm-build/Release+Asserts/bin:$PATH
 
 export GN_DEFINES='is_component_build=true symbol_level=1 dcheck_always_on=true'
 export GN_DEFINES=$GN_DEFINES' enable_nacl=false treat_warnings_as_errors=false'
 export GN_DEFINES=$GN_DEFINES' proprietary_codecs=true ffmpeg_branding="Chrome"'
-export GN_DEFINES=$GN_DEFINES' use_debug_fission=false linux_use_bundled_binutils=false clang_use_chrome_plugins=false cc_wrapper="icecc" ffmpeg_use_atomics_fallback=true enable_swiftshader=false '
+export GN_DEFINES=$GN_DEFINES' use_debug_fission=false linux_use_bundled_binutils=false clang_use_chrome_plugins=false cc_wrapper="icecc" ffmpeg_use_atomics_fallback=true enable_swiftshader=false use_jumbo_build = true '
 export GN_DEFINES=$GN_DEFINES' google_api_key="???" google_default_client_id="??.com" google_default_client_secret="??"'
 echo "GN_DEFINES: "$GN_DEFINES
 
