826f77ad284d4c504e26acce586b2e77cba7e7c3
Gyuyoung Kim <gyuyoung.kim@lge.com>
2018-01-05 00:12:27 +0900

    Update buildChromium.sh

--- buildChromium.sh
+++ buildChromium.sh
@@ -12,7 +12,7 @@ export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
 export CCACHE_BASEDIR=$HOME/chromium
 export ICECC_CLANG_REMOTE_CPP=1
 export ICECC_VERSION=$HOME/chromium/clang.tar.gz
-export CHROMIUM_SRC=$HOME/chromium/src # Please set the chromium src path in your local.
+export CHROMIUM_SRC=$HOME/chromium/src
 export PATH=/usr/lib/ccache:/usr/lib/icecc/bin:$PATH
 export PATH=$CHROMIUM_SRC/third_party/llvm-build/Release+Asserts/bin:$PATH
 
@@ -21,7 +21,9 @@ export GN_DEFINES=$GN_DEFINES' enable_nacl=false treat_warnings_as_errors=false'
 export GN_DEFINES=$GN_DEFINES' proprietary_codecs=true ffmpeg_branding="Chrome"'
 export GN_DEFINES=$GN_DEFINES' use_debug_fission=false linux_use_bundled_binutils=false clang_use_chrome_plugins=false cc_wrapper="icecc" ffmpeg_use_atomics_fallback=true enable_swiftshader=false use_jumbo_build = true '
 export GN_DEFINES=$GN_DEFINES' google_api_key="???" google_default_client_id="??.com" google_default_client_secret="??"'
-echo "GN_DEFINES: "$GN_DEFINES
+timestamp=$(date +"%T")
+echo "[$timestamp] 1. Configuration"
+#echo "GN_DEFINES: "$GN_DEFINES
 
 if [ "$1" == Debug ];
 then
@@ -29,11 +31,14 @@ then
 else
   gn gen out/Release "--args=is_debug=false $GN_DEFINES"
 fi
+echo ""
 
-echo "============================================="
-timestamp=$(date +"%T")
-echo "[$timestamp] Start compiling Chromium on $1 mode"
+start_timestamp=$(date +"%T")
+start_time=$(date +"%S")
+echo "[$start_timestamp] 2. Start compiling Chromium on $1 mode"
 ninja -j 100 -C out/"$1" chrome $2
-timestamp=$(date +"%T")
-echo "[$timestamp] Finish to compile Chromium!"
-echo "============================================="
+end_timestamp=$(date +"%T")
+end_time=$(date +"%S")
+echo ""
+diffTime=`expr ${end_time} - ${start_time}`
+echo "[$end_timestamp] 3. Finish to compile Chromium (Elapsed time: $(($diffTime / 60)) minutes and $(($duration % 60)) seconds.)"
