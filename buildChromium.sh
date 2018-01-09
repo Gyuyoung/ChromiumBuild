d530b63a0c01feebbc30917d4e1e2cc7a82b8bd0
Gyuyoung Kim <gyuyoung.kim@lge.com>
2018-01-09 18:21:20 +0900

    Update buildChromium.sh

--- buildChromium.sh
+++ buildChromium.sh
@@ -16,20 +16,25 @@ export CHROMIUM_SRC=$HOME/chromium/src
 export PATH=/usr/lib/ccache:/usr/lib/icecc/bin:$PATH
 export PATH=$CHROMIUM_SRC/third_party/llvm-build/Release+Asserts/bin:$PATH
 
-export GN_DEFINES='is_component_build=true symbol_level=1 dcheck_always_on=true'
+export GN_DEFINES='is_component_build=true'
 export GN_DEFINES=$GN_DEFINES' enable_nacl=false treat_warnings_as_errors=false'
 export GN_DEFINES=$GN_DEFINES' proprietary_codecs=true ffmpeg_branding="Chrome"'
 export GN_DEFINES=$GN_DEFINES' use_debug_fission=false linux_use_bundled_binutils=false clang_use_chrome_plugins=false cc_wrapper="icecc" ffmpeg_use_atomics_fallback=true enable_swiftshader=false use_jumbo_build = true '
 export GN_DEFINES=$GN_DEFINES' google_api_key="???" google_default_client_id="??.com" google_default_client_secret="??"'
 timestamp=$(date +"%T")
 echo "[$timestamp] 1. Configuration"
-#echo "GN_DEFINES: "$GN_DEFINES
 
 if [ "$1" == Debug ];
 then
+  export GN_DEFINES='dcheck_always_on=true'
+  echo "GN_DEFINES: "$GN_DEFINES
   gn gen out/Debug "--args=is_debug=true $GN_DEFINES"
-else
+elif [ "$1" == Release ];
+then
+  echo "GN_DEFINES: "$GN_DEFINES
   gn gen out/Release "--args=is_debug=false $GN_DEFINES"
+else
+  echo "Undefined Debug or Release."
 fi
 echo ""
 
